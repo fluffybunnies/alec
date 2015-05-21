e5b7f156e90080b808fbff8858b3e6e87ab4e16e
alec <alec.hulce@gmail.com>
2015-05-20 21:00:14 -0700

    ヽ༼ຈل͜ຈ༽ง

--- utils/parse_tickets.sh
+++ utils/parse_tickets.sh
@@ -2,8 +2,7 @@
 # ./run.sh v0.3.163_release..v0.3.179_release
 #
 # @todo: try just sedding this cuz this loop is sloooow
-# sed -n -E '/.*\(TLG-[0-9]+\).*/\1/p' $log
-# echo $log | sed -n -E '/.*\(TLG\-[0-9]+\).*/X/p'
+# echo -e "[TLG-1234] stuff\n[TLG-5678] more stuff" | sed -E -n 's/.*(TLG-[0-9]+).*/\1/gp'
 #
 
 
@@ -15,16 +14,28 @@ isInt(){
 }
 
 
-log=`git log --pretty=format:%s $1`
-log=`echo $log | sed -E -e 's/[^TLG0-9\-]+/ /g'`
-
+out=
+log=`git log --pretty=format:%s $1 | sed -E -n 's/.*(TLG-[0-9]+).*/\1/gp'`
+for line in $log; do
+	lineN=`echo $line | sed -E -n 's/TLG-([0-9]+)$/\1/gp'`
+	if [ "${saved[$lineN]}" == "" ]; then
+		out=$out'\n'$line
+		saved[line]=1
+	fi
+done
+echo
+echo -e $out | sort
 exit
+
+
+log=`git log --pretty=format:%s $1`
 
 #exit
 #real	0m2.895s
 #user	0m1.205s
 #sys	0m1.660s
 
+out=
 len=${#log}
 for ((i=0;i<$len;++i)); do
 	c=${log:$i:1}
@@ -39,11 +50,14 @@ for ((i=0;i<$len;++i)); do
 		bufN=$bufN$c
 	else
 		if [ $l -gt 4 ] && [ "${bufs[$bufN]}" == "" ]; then
-			echo $buf
+			#echo $buf
+			echo -n '.'
+			out=$out'\n'$buf
 			bufs[bufN]=1
 		fi
 		buf=
 		bufN=
 	fi
 done
-
+echo
+echo -e $out | sort
